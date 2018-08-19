bdf8d5f4c1666dcc13cca03a654a900f01e36889
smartworld1004 <smartworld1004@outlook.com>
2018-08-19 12:44:11 +0700

    change: new JS

--- example/index.js
+++ example/index.js
@@ -10,7 +10,8 @@ var port = process.env.PORT || 8080;
 // serve the html/index.html
 app.use(express.static(path.resolve(__dirname, 'html')))
 // serve the player
-app.use(express.static(path.resolve(__dirname, '../lib')))
+// app.use(express.static(path.resolve(__dirname, '../lib')))
+app.use(express.static(path.resolve(__dirname, '../lib_new')))
 
 const server = http.createServer(app)
 
